b0901261f76e5ba27bdfd2a32360be7f4d5c34dd
OurdiaD <daideche.o@gmail.com>
2021-05-16 14:26:38 +0200

    floating button

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ShowNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ShowNeighbourActivity.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +23,7 @@ import com.openclassrooms.entrevoisins.model.Neighbour;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ShowNeighbourActivity extends AppCompatActivity {
     @BindView(R.id.imageView)
@@ -36,6 +40,11 @@ public class ShowNeighbourActivity extends AppCompatActivity {
     TextView listMail;
     @BindView(R.id.contentAbout)
     TextView contentAbout;
+    @BindView(R.id.add_fav)
+    FloatingActionButton addFav;
+
+    int colorFav;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,10 +75,19 @@ public class ShowNeighbourActivity extends AppCompatActivity {
             listTel.setText(neighbour.getPhoneNumber());
             listMail.setText(facebook);
             contentAbout.setText(neighbour.getAboutMe());
-
         }
+        colorFav = getResources().getColor(R.color.colorFav);
+        Drawable star = getDrawable(R.drawable.ic_star_border_white_24dp);
+        star.setColorFilter(colorFav, PorterDuff.Mode.SRC_IN);
+        addFav.setImageDrawable(star);
+    }
 
-
+    @OnClick(R.id.add_fav)
+    protected void addFavorite() {
+        //int color = getResources().getColor(R.color.colorFav);
+        Drawable star = getDrawable(R.drawable.ic_star_white_24dp);
+        star.setColorFilter(colorFav, PorterDuff.Mode.SRC_IN);
+        addFav.setImageDrawable(star);
     }
 
 }
